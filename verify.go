baeb8e3dd1594988645f55003bef6b23aed82a68
AbdullaYerezhep <yerezhepabdulla@gmail.com>
2023-12-17 21:01:50 +0500

    fix: use verifyInput struct in input

--- verify.go
+++ verify.go
@@ -2,20 +2,41 @@ package gokalkan
 
 import (
 	"encoding/base64"
+
 	"github.com/gokalkan/gokalkan/types"
 
 	"github.com/gokalkan/gokalkan/ckalkan"
 )
 
 // Verify обеспечивает проверку подписи CMS в base64.
-func (cli *Client) Verify(signature []byte) (string, error) {
-	signatureB64 := base64.StdEncoding.EncodeToString(signature)
-	flags := ckalkan.FlagSignCMS | ckalkan.FlagInBase64 | ckalkan.FlagOutBase64
+func (cli *Client) Verify(input *types.VerifyInput) (string, error) {
+	var (
+		flags             ckalkan.Flag
+		signatureAsBase64 string
+		dataAsBase64      string
+	)
 
-	resp, err := cli.kc.VerifyData(signatureB64, "", "", flags)
+	signatureAsBase64 = base64.StdEncoding.EncodeToString(input.SignatureBytes)
+	flags = ckalkan.FlagSignCMS | ckalkan.FlagInBase64
+
+	if input.IsDetached {
+		flags |= ckalkan.FlagIn2Base64
+		flags |= ckalkan.FlagDetachedData
+
+		dataAsBase64 = base64.StdEncoding.EncodeToString(input.DataBytes)
+	} else {
+		flags |= ckalkan.FlagOutBase64
+	}
+
+	if !input.MustCheckCertTime {
+		flags |= ckalkan.FlagNoCheckCertTime
+	}
+
+	resp, err := cli.kc.VerifyData(signatureAsBase64, dataAsBase64, "", flags)
 	if err != nil {
 		return "", err
 	}
+
 	return string(resp.Info), err
 }
 
