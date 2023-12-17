238709b3947e293256f1deb50138c41720cd9337
ymoldabe <MRanle91@gmail.com>
2023-12-17 19:41:46 +0600

    fix: Correct ValidateCert and ValidateCertOCSP methods

--- validate_cert.go
+++ validate_cert.go
@@ -7,16 +7,26 @@ import (
 func (cli *Client) ValidateCert(cert string) (string, error) {
 	validateType := ckalkan.ValidateTypeNothing
 	validatePath := ""
-	return cli.kc.X509ValidateCertificate(cert, validateType, validatePath)
+
+	var flags ckalkan.Flag
+
+	flags |= ckalkan.FlagNoCheckCertTime
+
+	return cli.kc.X509ValidateCertificate(cert, validateType, validatePath, flags)
 }
 
 func (cli *Client) ValidateCertOCSP(cert string, url ...string) (string, error) {
 	validateType := ckalkan.ValidateTypeOCSP
 	validatePath := cli.o.OCSP
 
+	var flags ckalkan.Flag
+
+	flags |= ckalkan.FlagNoCheckCertTime
+
 	if len(url) > 0 {
 		validatePath = url[0]
 	}
 
-	return cli.kc.X509ValidateCertificate(cert, validateType, validatePath)
+	return cli.kc.X509ValidateCertificate(cert, validateType, validatePath, flags)
+
 }
