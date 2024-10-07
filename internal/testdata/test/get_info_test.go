6d7676cd3203cc26d3655a49c0e347a48fba1ee9
Timur A. Issanov <timur.issanov@gmail.com>
2024-10-07 13:42:46 +0500

    fix: removed extra newline

--- internal/testdata/test/get_info_test.go
+++ internal/testdata/test/get_info_test.go
@@ -8,7 +8,6 @@ import (
 
 func TestX509CertificateGetInfo(t *testing.T) {
 	for _, key := range keys {
-
 		t.Run(key.Alias, func(tt *testing.T) {
 			_, err := cli.X509CertificateGetInfo(key.Cert, ckalkan.CertPropCertCN)
 			if err != nil {
